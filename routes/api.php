7c93cff5fdc0942db10dd45c6d93ac8b79848961
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-08-20 16:47:44 -0500

    feat (api) : Add logout route

--- routes/api.php
+++ routes/api.php
@@ -9,3 +9,6 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
 // Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+});
