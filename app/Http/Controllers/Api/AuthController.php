5e185315ea0cee55ebf70afe1ceb527b5711594e
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-08-22 10:20:48 -0500

    feat (AuthController) : Create Recovery password function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -9,6 +9,10 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Password;
+use Illuminate\Auth\Events\PasswordReset;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -95,5 +99,47 @@ class AuthController extends Controller
         $cookie = Cookie::forget('cookie_token');
 
         return response(["message" => "Logout Ok"], Response::HTTP_OK)->withCookie($cookie);
+    }
+
+    // Forgot password
+    public function forgotPassword(Request $request)
+    {
+        $request->validate(['email' => 'required|email']);
+
+        $status = Password::sendResetLink(
+            $request->only('email')
+        );
+
+        return $status === Password::RESET_LINK_SENT
+            ? response()->json(['message' => __($status)], Response::HTTP_OK)
+            : response()->json(['message' => __($status)], Response::HTTP_BAD_REQUEST);
+    }
+
+
+    // Reset Password
+    public function resetPassword(Request $request)
+    {
+        $request->validate([
+            'token' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|min:8|confirmed',
+        ]);
+
+        $status = Password::reset(
+            $request->only('email', 'password', 'password_confirmation', 'token'),
+            function (User $user, string $password) {
+                $user->forceFill([
+                    'password' => Hash::make($password)
+                ])->setRememberToken(Str::random(60));
+
+                $user->save();
+
+                event(new PasswordReset($user));
+            }
+        );
+
+        return $status === Password::PASSWORD_RESET
+            ? response()->json(['message' => __($status)], Response::HTTP_OK)
+            : response()->json(['message' => [__($status)]], Response::HTTP_BAD_REQUEST);
     }
 }
