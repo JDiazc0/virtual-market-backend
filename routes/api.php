786f9c3383dd4c1e7d6e676dcf995b005825f39a
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-08-24 22:04:48 -0500

    feat (api) : Add product routes

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductController;
 
 // Public routes
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
+Route::get('products', [ProductController::class, 'getProducts']);
+Route::get('products/{id}', [ProductController::class, 'getProduct']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
