5af5ccd02e4f411d780ac8e55f4605e8590d7f1e
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-08-25 11:47:07 -0500

    feat (model) : Create model promotion

--- app/Models/promotion.php
+++ app/Models/promotion.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class promotion extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'promotion_name',
+        'percentage',
+        'id_product'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class, 'id_product');
+    }
+
+    public function stores()
+    {
+        return $this->belongsToMany(Store::class, 'store_promotions', 'id_promotion', 'id_store')
+            ->withPivot('promotion_status', 'start_date', 'end_date')
+            ->withTimestamps();
+    }
 }
