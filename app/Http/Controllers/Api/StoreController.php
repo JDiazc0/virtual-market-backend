27fcdedcefadb3a5fff4bfcd6a68b06897bd4717
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-08-26 15:03:11 -0500

    feat (StoreContrller) : create store controller

---
+++ app/Http/Controllers/Api/StoreController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\store;
+use App\Models\store_product;
+use App\Models\store_promotion;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class StoreController extends Controller
+{
+    // Get stores
+    public function getStores()
+    {
+
+        $stores = Store::all();
+
+        return response()->json($stores, Response::HTTP_OK);
+    }
+
+    // Get products store
+    public function getStore($id)
+    {
+
+        $stores = Store::with('products')->findOrFail($id);
+
+        return response()->json($stores, Response::HTTP_OK);
+    }
+
+    // Add products to store
+    public function addProducts(Request $request)
+    {
+        // Validate data
+        $request->validate(
+            [
+                'id_product' => 'required',
+                'id_store' => 'required',
+                'amount' => 'required|numeric'
+            ]
+        );
+
+        // Create a new store product
+        $storeProduct = new store_product();
+        $storeProduct->id_product = $request->id_product;
+        $storeProduct->id_store = $request->id_store;
+        $storeProduct->amount = $request->amount;
+        $storeProduct->save();
+
+        return response()->json($storeProduct, Response::HTTP_CREATED);
+    }
+
+    // Add promotions to store 
+    public function addPromotions(Request $request)
+    {
+        // Validate data
+        $request->validate(
+            [
+                'id_promotion' => 'required',
+                'id_store' => 'required',
+                'start_date' => 'required',
+                'end_date' => 'required',
+                'promotion_status' => 'required',
+            ]
+        );
+
+        // Create a new store product
+        $storePromotion = new store_promotion();
+        $storePromotion->id_promotion = $request->id_promotion;
+        $storePromotion->id_store = $request->id_store;
+        $storePromotion->amount = $request->amount;
+        $storePromotion->save();
+
+        return response()->json($storePromotion, Response::HTTP_CREATED);
+    }
+
+    // Deactivate promotion
+    public function deactivatePromotion($id)
+    {
+        $storePromotion = store_promotion::findOrFail($id);
+        $storePromotion->promotion_status = false;
+        $storePromotion->save();
+
+        return response()->json(['message' => 'Promotion deactivated successfully'], Response::HTTP_OK);
+    }
+
+    // Activate promotion
+    public function activatePromotion($id)
+    {
+        $storePromotion = store_promotion::findOrFail($id);
+        $storePromotion->promotion_status = true;
+        $storePromotion->save();
+
+        return response()->json(['message' => 'Promotion activated successfully'], Response::HTTP_OK);
+    }
+}
