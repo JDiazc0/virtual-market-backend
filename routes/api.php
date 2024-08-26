250063ea3ce901ab6e6e2ebe3b765f2ee85880be
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-08-26 15:20:59 -0500

    feat (api) : Add Store routes

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\StoreController;
 
 // Public routes
 Route::post('register', [AuthController::class, 'register']);
@@ -12,8 +13,18 @@ Route::post('forgot-password', [AuthController::class, 'forgotPassword']);
 Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('password.reset');
 Route::get('products', [ProductController::class, 'getProducts']);
 Route::get('products/{id}', [ProductController::class, 'getProduct']);
+Route::get('stores', [StoreController::class, 'getStores']);
+Route::get('store/{id}', [StoreController::class, 'getStore']);
 
 // Protected routes
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::post('logout', [AuthController::class, 'logout']);
+
+    // Protected store routes
+    Route::group(['middleware' => ['userType:store'], 'prefix' => 'store'], function () {
+        Route::post('add-products', [StoreController::class, 'addProducts']);
+        Route::post('add-promotions', [StoreController::class, 'addPromotions']);
+        Route::post('active-promotions/{id}', [StoreController::class, 'activatePromotion']);
+        Route::post('deactive-promotions/{id}', [StoreController::class, 'deactivatePromotion']);
+    });
 });
