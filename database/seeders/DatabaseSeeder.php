51d0cf1e3091d928ae5d0942e5919d2f6e3048dc
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-09-02 15:20:48 -0500

    feat (seeder): Create order statusses seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
             UserTypeSeeder::class,
             ProductsSeeder::class,
             PromotionsSeeder::class,
+            OrderStatussesSeeder::class,
         ]);
     }
 }
