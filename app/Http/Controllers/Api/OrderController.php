b7567c0f97c4dabd52353f71936393166c7bcd5b
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-09-03 08:44:24 -0500

    feat (OrderController): Create function createOrder

---
+++ app/Http/Controllers/Api/OrderController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\shopping_cart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpFoundation\Response;
+use App\Helpers\DiscountCalculator;
+use App\Models\order;
+use App\Models\ordered_item;
+
+class OrderController extends Controller
+{
+    // Create Order
+    public function createOrder(Request $request)
+    {
+        $request->validate([
+            'id_user' => 'required|exists:users,id',
+            'id_store' => 'required|exists:stores,id',
+            'instructions' => 'nullable|string',
+            'delivery_date' => 'required',
+            'address' => 'required',
+            'rate' => 'required|min:1|max:5'
+        ]);
+
+        try {
+            DB::beginTransaction();
+
+            // Get all cart products
+            $cartItems = shopping_cart::where('id_user', $request->id_user)
+                ->where('id_store', $request->id_store)
+                ->with('product')
+                ->get();
+
+            if ($cartItems->isEmpty()) {
+                return response()->json(['error' => 'Cart is empty'], Response::HTTP_BAD_REQUEST);
+            }
+
+            // Total cost of products
+            $productValue = $cartItems->sum(function ($item) {
+                return $item->amount * $item->product->price;
+            });
+
+            // Product discounts + store discounts
+            $discountValue = DiscountCalculator::calculateDiscounts($cartItems, $request->id_store);
+
+            // Shipping cost
+            $shippingValue = 500;
+
+            // Tax cost
+            $taxValue = $productValue * .19;
+
+            // Final cost
+            $finalValue = $productValue + $shippingValue - $discountValue;
+
+            // Create order
+            $order = order::create([
+                'id_store' => $request->id_store,
+                'id_user' => $request->id_user,
+                'instructions' => $request->instructions,
+                'delivery_date' => $request->delivery_date,
+                'product_value' => $productValue,
+                'shipping_value' => $shippingValue,
+                'discount_value' => $discountValue,
+                'taxes_value' => $taxValue,
+                'final_value' => $finalValue,
+                'address' => $request->address,
+                'rate' => $request->rate,
+                'id_status' => 1,
+            ]);
+
+            // Create ordered items
+            foreach ($cartItems as $cartItem) {
+                $result = DiscountCalculator::calculateDiscountedPrice($cartItem->product, $request->id_store);
+
+                // Datos para el `ordered_item`
+                $unitValue = $cartItem->product->price;
+                $discountedUnitValue = $result['price'];
+                $finalValue = $discountedUnitValue * $cartItem->amount;
+                $discountValue += ($unitValue - $discountedUnitValue) * $cartItem->amount;
+                $listPrice = $unitValue * $cartItem->amount;
+
+                // Crear `ordered_item`
+                ordered_item::create([
+                    'id_product' => $cartItem->id_product,
+                    'id_order' => $order->id,
+                    'id_promotion' => $result['promotion_id'],
+                    'amount' => $cartItem->amount,
+                    'unit_value' => $unitValue,
+                    'discounted_unit_value' => $discountedUnitValue,
+                    'list_price' => $listPrice,
+                    'final_value' => $finalValue,
+                ]);
+            }
+
+            // Clear the cart
+            shopping_cart::where('id_user', $request->id_user)
+                ->where('id_store', $request->id_store)
+                ->delete();
+
+            DB::commit();
+
+            return response()->json(['order' => $order], Response::HTTP_OK);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json(['error' => 'Error creating the order' . $e->getMessage()], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+    }
+}
