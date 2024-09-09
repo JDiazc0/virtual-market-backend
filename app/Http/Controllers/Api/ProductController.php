a39fce1a012e042473e3aeb8e8c6fff6b7979c04
JDiazc0 <diaz.jhoan@correounivalle.edu.co>
2024-09-09 15:33:33 -0500

    feat (Controller): Create search products function

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\SearchProductRequest;
 use App\Models\product;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -23,5 +24,35 @@ class ProductController extends Controller
         $product = Product::with('stores')->findOrFail($id);
 
         return response()->json($product, Response::HTTP_OK);
+    }
+
+    // Search products
+    public function searchProducts(SearchProductRequest $request)
+    {
+
+        $query = Product::query();
+
+        // Filter by name
+        if ($request->filled('name')) {
+            $query->where('product_name', 'LIKE', '%' . $request->name . '%');
+        }
+
+        // Filter by presentation
+        if ($request->filled('presentation')) {
+            $query->where('presentation', 'LIKE', $request->presentation);
+        }
+
+        // Filter by price
+        if ($request->filled('priceRange')) {
+            $priceRange = explode('-', $request->input('priceRange'));
+            $minPrice = $priceRange[0];
+            $maxPrice = $priceRange[1];
+
+            $query->whereBetween('price', [$minPrice, $maxPrice]);
+        };
+
+        $products = $query->get();
+
+        return response()->json(['Products' => $products], Response::HTTP_OK);
     }
 }
